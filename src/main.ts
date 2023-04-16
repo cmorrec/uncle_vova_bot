4c4ca31ea3ff79891273f93300b404f0d448fd47
Artem Katnov <akatnov@upteam.com>
2023-04-16 18:40:02 +0200

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { getBotToken } from 'nestjs-telegraf';
 import { AppModule } from './app.module';
+import { EnvironmentVariables } from './env-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
+  const app = await NestFactory.create(AppModule);
+  const configService: ConfigService<EnvironmentVariables> =
+    app.get(ConfigService);
+  const webhookPath = configService.get<string>('WEBHOOK_PATH');
+
+  if (webhookPath) {
+    const bot = app.get(getBotToken());
+    app.use(bot.webhookCallback(webhookPath));
+  } else {
+    await app.listen(8080);
+  }
 }
 bootstrap();
